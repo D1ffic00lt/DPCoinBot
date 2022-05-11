ab62ab0da27024f9805d54aa309b636f9eaf1344
D1ffic00lt <dm.filinov@gmail.com>
2022-05-11 12:03:20 +0700

    add config

---
+++ config.py
@@ -0,0 +1,9 @@
+settings = {
+    'token': 'token',
+    'bot': 'DPcoin BOT',
+    'id': 727526646223208549,
+    'prefix': '//',
+    "password": "password1",
+    "sender_email": "sender email",
+    "to_send_email": "to email"
+}
