c883c231a560ccee985638edcede4ec97c086580
D1ffic00lt <dm.filinov@gmail.com>
2022-05-13 21:46:18 +0700

    добавлены новые функции

---
+++ main/bot.py
@@ -0,0 +1,23 @@
+import math
+
+from discord.ext import commands
+
+from ..database.db import Database
+
+
+class DPcoinBOT(commands.Bot):
+    def __init__(self, command_prefix, **options):
+        super().__init__(command_prefix, **options)
+        self.db = Database("server.db")
+
+    async def on_ready(self):
+        self.db.server_add()
+        if not self.db.checking_for_levels_existence_in_table():
+            lvl = 1
+            for i in range(1, 405):
+                self.db.insert_into_levels(i, int(math.pow((lvl * 32), 1.4)), i * int(math.pow(lvl, 1.2)))
+                lvl += 1
+            self.db.cursor.execute("UPDATE levels SET award = 1500000 WHERE level = 404")
+            self.db.connection.commit()
+
+        self.db.clear_coinflip()
---
+++ main/main.py
@@ -0,0 +1,31 @@
+import discord
+
+from discord.ext import commands
+
+from DPcoinBOTs.DPcoinBOT2.templates.helperfunction import *
+from DPcoinBOTs.DPcoinBOT2.config import *
+from DPcoinBOTs.DPcoinBOT2.version import __version__
+from DPcoinBOTs.DPcoinBOT2.main.bot import DPcoinBOT
+
+
+def main() -> None:
+    BOT = DPcoinBOT(
+        command_prefix=settings["prefix"],
+        intents=discord.Intents.all()
+    )
+
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(
+        f"{settings['prefix']}help"))
+
+    # if not os.path.exists("json_/ban_list.json"):
+    #     with open('json_/last_save.json', 'w+') as outfile:
+    #         json.dump([], outfile)
+
+    print("Bot connected")
+    print("version: {}\n".format(__version__))
+    BOT.add_cog()
+    BOT.run(settings["token"])
+
+
+if __name__ == '__main__':
+    main()
