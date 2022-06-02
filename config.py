21361bdc865222798f722c4c2c127f2cbd0b886d
D1ffic00lt <dm.filinov@gmail.com>
2022-06-02 22:26:05 +0700

    stats

--- config.py
+++ config.py
@@ -5,11 +5,5 @@ settings = {
     'webhook': 'webhook',
     'test_webhook': 'test_webhook',
     'owner_id_group': "owner_id: int",
-    'bot': 'DPcoin BOT',
-    'id': 727526646223208549,
-    'prefix': '//',
-    "password": "password1",
-    "sender_email": "sender email",
-    "to_send_email": "to email",
-    "casino": [20, 3, 5, 1, 3, 1, 10, 1, 3, 5, 1, 5, 3, 10, 1, 3, 5, 1, 3, 1, 1]
+    'bot': 'DPcoin BOT'
 }
