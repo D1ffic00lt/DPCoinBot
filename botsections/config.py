b220d5f537f93e2b1c58bb91af5c590f527894ea
D1ffic00lt <dm.filinov@gmail.com>
2022-11-04 15:46:09 +0700

    encoded config

--- botsections/config.py
+++ botsections/config.py
@@ -1,14 +1,29 @@
 # -*- coding: utf-8 -*-
+"""
+    Да, я конч, спалил все токены, пароли и всякое такое,
+    но я исправился! Я поменял все токены и пароли, к тому же, они теперь шифруются
+    можете теперь даже не пытаться
+"""
 settings = {
-    'token': 'NzY1ODI1OTczNjAxNjk3ODE1.X4adBA.UHbXJCxGz4PAT76PGJjZhG1apsA',
-    'vk_token': '304cf3f099eb29914e850e0956481672f17aa2517faca1b2c791842b399cad31637c07164fa4d1f56e658',
-    'webhook': 'webhook',
-    'test_webhook': 'test_webhook',
-    'owner_id_group': "owner_id: int",
-    'bot': 'DPcoin BOT',
-    'id': 727526646223208549,
+    'token': b'gAAAAABjZM5mNSr8tZSUBQx15CyMPwyj_fiEo4HbJnOeLSYYQBF6Svnj0ZGj8gx1o'
+             b'TFWzd9CWmV-OOioJxwSv4U7N0BkZYLRikB_oNRTNinB3djaNWEp_abirS1A5sJB4K'
+             b'0_AxZp333zQCHSUBR3rY1gfFRzwletOOUxgbaJIEBZPBfymHJ4nMavMWLfgMMn1jL'
+             b'yvtqaPmTC-EOZSSblgiimGdFgnNtgfA==',
+    'vk_token': b'gAAAAABjZM_E-hsfiBaZRos2aEU5R0rJlNzCVj1HkS6sQgABEdk1SOn0DYEKMQ'
+                b'2jz5wBSwMzd8Yv6XTOrCrb6HolYhUR6OlwP9VDZIEStH_Nxclg_HuD7CmO9Y5i'
+                b'kI_gnTLK3qaJMWdrrT2CPEcuKmuWoYE3255cfKKbUT9SlWzEm_GUBsKuMD-E34'
+                b'gU5no01oMTEOG51CyRrhU3J_s33oKMBfMpqGHP6cyP0bA1HWJ3K5ReNFQJ28o=',
+    'webhook': 'webhook',                   # пока не требуется
+    'test_webhook': 'test_webhook',         # аналогично
+    'owner_id_group': "owner_id: int",      # аналогично
+    'bot': 'DPcoin BOT',                    # это итак все знают
+    'id': b'gAAAAABjZM_yhaE17Ml7_uWwsp3qcOqf1u2g5_y47mhEmjmdCrhOosd7VAFk'
+          b'wOJdfnRfd7GOl_H_D7ACEAQ0SU7x1O2Sa5VGjWByEWYEnW_ZpIrokGXNvkg=',
     'prefix': '//',
-    "password": "Dom2Govno",
-    "sender_email": "dpcoinbot@gmail.com",
-    "to_send_email": "dm.filinov@gmail.com"
+    "password": b'gAAAAABjZNBwHXsuUCEWpnJssOOMYkmNta-WdHOYWh3DsjoD3uvAJ4JQql-p'
+                b'lYCCMh-tur2yJA6hWk9h1zBJiprZU0TetMMD3ZNcx7RdJHfARWJXCSq-lCY=',
+    "sender_email": b'gAAAAABjZNADXnn912gRHsi-VJdTOdwdURimJ3gLB_HhARaxStglvAm6hoNH'
+                    b'8_q-1e-FZLpUpzH65t4moKjkYLAGCBi0meIAmoNYdNpcfcb9FTfQ6lHQCOQ=',
+    "to_send_email": b'gAAAAABjZNASo0D8yoYUkxlIyJncwUg-rb_N6qubnhuuj4Q9TtLn6aEqjwyQ'
+                     b'gBpeOQ-YFnA_-x2V61LApxNczB6CEvkVXOrhSX0P3pE-5RLvd4B8bjYlMfA='
 }
