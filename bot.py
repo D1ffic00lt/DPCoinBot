381a01c1ef3a402e7afc6335baf0821a4091c1d9
Dmitry Filinov <dm.filinov@gmail.com>
2023-05-13 17:14:20 +0700

    imports fixed

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from database.user.users import User
 from database.user.cards import Card
 from database.user.achievements import Achievement
 from database.user.inventories import Inventory
-from .database.user.stats import UserStats
+from database.user.stats import UserStats
 from database.bot.new_year_event import NewYearEvent
 from database.bot.levels import Level
 from database.bot.coinflips import CoinFlip
