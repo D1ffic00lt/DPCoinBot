b5fd24dea5c2a2c04caad2605a1a3af00cb50966
Dmitry Filinov <dm.filinov@gmail.com>
2023-05-19 22:02:20 +0700

    the valentines_dat.py (slash) file has been transferred to the orm database format

--- units/slash_units/global_events/valentines_day.py
+++ units/slash_units/global_events/valentines_day.py
@@ -3,10 +3,13 @@ import logging
 import random
 
 from datetime import datetime
+from typing import Callable
 
 import discord
 from discord import app_commands
 from discord.ext import commands
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from config import VALENTINES_DAY_MIN_PRIZE, VALENTINES_DAY_MAX_PRIZE
 
@@ -14,66 +17,69 @@ __all__ = (
     "ValentinesDaySlash",
 )
 
+from database.user.users import User
+
 
 class ValentinesDaySlash(commands.Cog):
     NAME = 'ValentinesDay slash module'
 
-    __slots__ = (
-        "bot", "db", "prize", "valentine"
-    )
-
-    def __init__(self, bot: commands.Bot, db, *args, **kwargs) -> None:
+    def __init__(self, bot: commands.Bot, session, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.bot: commands.Bot = bot
-        self.db = db
-        self.valentine: int = 0
-        self.prize: int = 0
+        self.session: Callable[[], AsyncSession] = session
         logging.info(f"ValentinesDay (Slash) event connected")
 
     @app_commands.command(name="val_open", description="Открыть валентинку")
     @commands.cooldown(1, 4, commands.BucketType.user)
     async def __val_open(self, inter: discord.Interaction, count: int = None) -> None:
-        if int(datetime.today().strftime('%m')) == 2 and (9 < int(datetime.today().strftime('%d')) < 15):
-            self.valentine = self.db.get_from_inventory(inter.user.id, inter.guild.id, "Valentines")
-            if self.valentine == 0:
-                await inter.response.send_message(
-                    "У Вас нет валентинок:(", ephemeral=True
-                )
-                return
-            if isinstance(count, int):
-                if int(count) > self.valentine:
-                    await inter.response.send_message(
-                        "У Вас недостаточно валентинок:(\nУ Вас {} валентинок".format(self.valentine),
-                        ephemeral=True
+        if not (int(datetime.today().strftime('%m')) == 2 and int(datetime.today().strftime('%d')) == 14):
+            return
+        async with self.session() as session:
+            async with session.begin():
+                user = await session.execute(
+                    select(User).where(
+                        User.user_id == inter.user.id and User.guild_id == inter.guild.id
                     )
+                )
+                user: User | None = user.scalars().first()
+                if not user:
                     return
-                elif int(count) <= 0:
+                valentine = user.inventories[0].valentines
+                if valentine == 0:
+                    await inter.response.send_message("У Вас нет валентинок:(")
+                    return
+                if isinstance(count, int):
+                    if int(count) > valentine:
+                        await inter.response.send_message(
+                            "У Вас недостаточно валентинок:(\nУ Вас {} валентинок".format(valentine)
+                        )
+                        return
+                    elif int(count) <= 0:
+                        await inter.response.send_message(
+                            f"{inter.user.mention}, Вы не можете отрыть 0(ну или меньше) валентинок:)"
+                        )
+                        return
+                if count is None:
+                    prize = random.randint(VALENTINES_DAY_MIN_PRIZE, VALENTINES_DAY_MAX_PRIZE)
+                    user.cash += prize
+                    user.inventories[0].valentines -= 1
                     await inter.response.send_message(
-                        f"{inter.user.mention}, Вы не можете отрыть 0(ну или меньше) валентинок:)",
-                        ephemeral=True
+                        f"{inter.user.mention}, из валентинки выпало {prize} коинов! Поздравляем!"
                     )
-                    return
-            if count is None:
-                self.prize = random.randint(VALENTINES_DAY_MIN_PRIZE, VALENTINES_DAY_MAX_PRIZE)
-                self.db.add_coins(inter.user.id, inter.guild.id, self.prize)
-                self.db.update_inventory(inter.user.id, inter.guild.id, "Valentines", -1)
-                await inter.response.send_message(
-                    f"{inter.user.mention}, из валентинки выпало {self.prize} коинов! Поздравляем!"
-                )
-            elif count == "all":
-                self.prize = random.randint(
-                    VALENTINES_DAY_MIN_PRIZE * self.valentine,
-                    VALENTINES_DAY_MAX_PRIZE * self.valentine
-                )
-                self.db.add_coins(inter.user.id, inter.guild.id, self.prize)
-                self.db.update_inventory(inter.user.id, inter.guild.id, "Valentines", -self.valentine)
-                await inter.response.send_message(
-                    f"{inter.user.mention}, из валентинок выпало {self.prize} коинов! Поздравляем!"
-                )
-            else:
-                self.prize = random.randint(VALENTINES_DAY_MIN_PRIZE * count, VALENTINES_DAY_MAX_PRIZE * count)
-                self.db.add_coins(inter.user.id, inter.guild.id, self.prize)
-                self.db.update_inventory(inter.user.id, inter.guild.id, "Valentines", -count)
-                await inter.response.send_message(
-                    f"{inter.user.mention}, из валентинок выпало {self.prize} коинов! Поздравляем!"
-                )
+                elif count == "all":
+                    prize = random.randint(
+                        VALENTINES_DAY_MIN_PRIZE * valentine,
+                        VALENTINES_DAY_MAX_PRIZE * valentine
+                    )
+                    user.cash += prize
+                    user.inventories[0].valentines -= valentine
+                    await inter.response.send_message(
+                        f"{inter.user.mention}, из валентинок выпало {prize} коинов! Поздравляем!"
+                    )
+                else:
+                    prize = random.randint(VALENTINES_DAY_MIN_PRIZE * count, VALENTINES_DAY_MAX_PRIZE * count)
+                    user.cash += prize
+                    user.inventories[0].valentines -= count
+                    await inter.response.send_message(
+                        f"{inter.user.mention}, из валентинок выпало {prize} коинов! Поздравляем!"
+                    )
